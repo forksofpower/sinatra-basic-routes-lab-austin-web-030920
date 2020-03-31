c2b194324dfecf516ae4942b67de4f923bb78ee9
Patrick Jones <patrickjones.pmj@gmail.com>
2020-03-31 15:23:29 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get "/name" do
+        "My name is Patrick"
+    end
+
+    get "/hometown" do
+        "My hometown is Austin"
+    end
+
+    get "/favorite-song" do
+       "My favorite song is Turtles All the Way Down by Sturgill Simpson" 
+    end
 end
